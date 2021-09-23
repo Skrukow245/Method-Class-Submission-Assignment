4416f3a29c85e77dc559bace1303b0a48afd969f
Skrukow245 <skrukow245@gmail.com>
2021-09-23 13:12:27 -0600

    Add project files.

---
+++ Method Class Submission Assignment/Math.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+public class Math
+{
+    public void Add(int input1, int input2, out int result)
+    {
+        result = input2;
+    }
+}
+
---
+++ Method Class Submission Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var addition = new Math();
+        int input1 = 5;
+        int input2 = 4;
+        int result;
+        addition.Add(input1, input2, out result);
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+
+}
+
